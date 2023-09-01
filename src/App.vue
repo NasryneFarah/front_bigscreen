f602689bc39dec18c7677caeec6cab7878b8a74e
Farah KONE <farahyedidiah@gmail.com>
2023-09-01 21:08:51 +0000

    ajout police matérialisation de la page

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,9 @@ import { RouterLink, RouterView } from 'vue-router'
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header>-->
 
-  <RouterView /> -->
+  <RouterView /> 
 </template>
 
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,146 @@
-<script></script>
+<script>
+
+</script>
 
 <template>
-    <div>
+ <!-- la video en background -->
+    <video autoplay loop muted id="bg_video">
+        <source src="public/assets/videos/Oculus.mp4" type="video/mp4">
+    </video>
+
+<!-- le logo du site -->
+    <div class="logo">
+        <img src="public/assets/images/logo_bigscreen.png" width="200">
+    </div>
+<!-- le texte d'arriver sur le site -->
+    <div class="text">
+        <h1>
+           Bienvenue à <br> Bigscreen
+        </h1>
+    </div>
+    <!-- flèche directionnelle -->
+    <div class="box">
+        <a href="#main">
+        <span></span>
+        <span></span>
+        <span></span>
+        </a>
         
     </div>
+
+    <!-- div pour accéder au sondage -->
+   <main id="main">
+
+   </main>
 </template>
 
 <style>
+
+*{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
+/* rajouter un effet lorsque je scroll avec mes flèches directionnelles */
+html{
+    scroll-behavior: smooth;
+}
+
+/*--importer mes polices*/
+@font-face {
+    font-family: 'Lunar';
+    src: url('public/assets/fonts/neuropolitical rg.otf');
+}
+/*--video*/
+#bg_video{
+    height: 100vh;
+    width: 100vw;
+    object-fit: cover;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: -1;
+}
+
+.logo img{
+    margin-left: 30px;
+    margin-top:15px ;
+}
+
+.text{
+    font-family: 'Lunar';
+    text-align: center;
+    font-size: 3.6rem;
+    color: #fff;
+    /* text-transform: uppercase; */
+    letter-spacing: 2px;
+    text-shadow: 3px 5px 2px #7089C0;
+    position: absolute;
+    margin: auto;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+}
+/* flèche directionnelle */
+.box{
+    cursor: pointer;
+    position: absolute;
+    top: 90%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+}
+
+.box span{
+    display: block;
+    width: 30px;
+    height: 30px;
+    border-bottom: 2px solid #7089C0;
+    border-right: 2px solid #7089C0;
+    margin: -10px;
+    transform: rotate(45deg);
+    animation: animate 2s infinite;
+}
+
+/* animation des arrows */
+.box span:nth-child(1){
+    animation-delay: -0.2s;
+    transform: rotate(45deg) translate(-20px,-20px);
+}
+
+.box span:nth-child(2){
+    animation-delay: -0.4s;
+}
+
+.box span:nth-child(3){
+    animation-delay: .2s;
+}
+@keyframes animate
+{
+    0%{
+        opacity: 0;
+        transform: rotate(45deg) translate(-20px,-20px);
+    }
+
+    50%{
+        opacity: 1;
+    }
+
+    100%{
+        opacity: 0;
+        transform: rotate(45deg) translate(20px,20px);
+    }
+}
+
+/* main */
+main{
+    position: absolute;
+    top: 110%;
+    width: 100%;
+    height: 100vh;
+    background: rgb(34,30,66);
+    background: linear-gradient(335deg, rgba(34,30,66,1) 25%, rgba(198,29,110,1) 90%);
+}
 </style>
